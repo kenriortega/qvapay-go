e273fd73881fc18c5a6267b15742521335d04b88
kenriortega <kenriortega@gmail.com>
2021-09-04 20:03:35 -0400

    feat: change objects responses and method retrns

--- types.go
+++ types.go
@@ -24,18 +24,18 @@ type Client interface {
 	// GetTrasactions ...
 	GetTransactions(
 		ctx context.Context,
-	) error
+	) (TransactionsResponse, error)
 
 	// GetTransaction ...
 	GetTransaction(
 		ctx context.Context,
 		id string,
-	) error
+	) (TransactionReponse, error)
 
 	// GetBalance ...
 	GetBalance(
 		ctx context.Context,
-	) (BalanceQvaPay, error)
+	) (BalanceQvaPayResponse, error)
 }
 
 // Client
@@ -45,10 +45,14 @@ type Client interface {
 // a timeout), assign to the HTTPClient field. To set a non-default URL (for
 // example, for testing), assign to the URL field.
 type client struct {
+	appID      string
+	secretID   string
 	url        string
 	httpClient *http.Client
 	debug      io.Writer
 }
+
+// Objects Responses
 
 // AppInfoResponse it`s an object that show general datail about your app
 type AppInfoResponse struct {
@@ -59,11 +63,12 @@ type AppInfoResponse struct {
 	Callback string `json:"callback,omitempty"`
 	Logo     string `json:"logo,omitempty"`
 	Uuid     string `json:"uuid,omitempty"`
-	Secret   string `json:"secret,omitempty"`
 	Active   int    `json:"active,omitempty"`
 	Enabled  int    `json:"enabled,omitempty"`
+	Secret   string `json:"secret,omitempty"`
 }
 
+// InvoiceResponse object
 type InvoiceResponse struct {
 	AppID           string  `json:"app_id,omitempty"`
 	Amount          float32 `json:"amount,omitempty"`
@@ -75,4 +80,94 @@ type InvoiceResponse struct {
 	SignedUrl       string  `json:"signedUrl,omitempty"`
 }
 
-type BalanceQvaPay string
+// BalanceQvaPayResponse string
+type BalanceQvaPayResponse string
+
+// TransactionsResponse reposnses
+type TransactionsResponse struct {
+	CurrentPage  int           `json:"current_page,omitempty"`
+	Data         []Transaction `json:"data,omitempty"`
+	FristPageURL string        `json:"frist_page_url,omitempty"`
+	From         int           `json:"from,omitempty"`
+	LastPage     int           `json:"last_page,omitempty"`
+	LastPageURL  string        `json:"last_page_url,omitempty"`
+	NextPageURL  string        `json:"next_page_url,omitempty"`
+	Path         string        `json:"path,omitempty"`
+	PerPage      int           `json:"per_page,omitempty"`
+	PrevPageURL  string        `json:"prev_page_url,omitempty"`
+	To           int           `json:"to,omitempty"`
+	Total        int           `json:"total,omitempty"`
+}
+
+// TransactionReponse object
+type TransactionReponse struct {
+	ID                string `json:"uuid,omitempty"`
+	UserID            string `json:"user_id,omitempty"`
+	AppID             string `json:"app_id,omitempty"`
+	Amount            string `json:"amount,omitempty"`
+	Description       string `json:"description,omitempty"`
+	RemoteID          string `json:"remote_id,omitempty"`
+	Status            string `json:"status,omitempty"`
+	PaidByUserID      int    `json:"paid_by_user_id,omitempty"`
+	Signed            int    `json:"signed,omitempty"`
+	CreatedAt         string `json:"created_at,omitempty"`
+	UpdatedAt         string `json:"updated_at,omitempty"`
+	TransactionPaidBy `json:"paid_by,omitempty"`
+	App               `json:"app,omitempty"`
+	Owner             `json:"owner,omitempty"`
+}
+
+// Models
+
+// Trasaction object
+type Transaction struct {
+	ID           string `json:"uuid,omitempty"`
+	UserID       string `json:"user_id,omitempty"`
+	AppID        string `json:"app_id,omitempty"`
+	Amount       string `json:"amount,omitempty"`
+	Description  string `json:"description,omitempty"`
+	RemoteID     string `json:"remote_id,omitempty"`
+	Status       string `json:"status,omitempty"`
+	PaidByUserID int    `json:"paid_by_user_id,omitempty"`
+	Signed       int    `json:"signed,omitempty"`
+	CreatedAt    string `json:"created_at,omitempty"`
+	UpdatedAt    string `json:"updated_at,omitempty"`
+}
+
+// TransactionPaidBy object
+type TransactionPaidBy struct {
+	Name string `json:"name,omitempty"`
+	Logo string `json:"logo,omitempty"`
+}
+
+// App object
+type App struct {
+	UserID   string `json:"user_id,omitempty"`
+	Name     string `json:"name,omitempty"`
+	URL      string `json:"url,omitempty"`
+	Desc     string `json:"desc,omitempty"`
+	Callback string `json:"callback,omitempty"`
+	Logo     string `json:"logo,omitempty"`
+	Uuid     string `json:"uuid,omitempty"`
+	Active   int    `json:"active,omitempty"`
+	Enabled  int    `json:"enabled,omitempty"`
+}
+
+// Owner object
+type Owner struct {
+	ID       string `json:"uuid,omitempty"`
+	Username string `json:"username,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Lastname string `json:"lastname,omitempty"`
+	Logo     string `json:"logo,omitempty"`
+}
+
+const (
+	ApiVersion   = "v1"
+	BaseURL      = "https://qvapay.com/api"
+	RouteInfo    = "info"
+	RouteInvoice = "create_invoice"
+	RouteTxs     = "transactions"
+	RouteTx      = "transaction"
+	RouteBalance = "balance"
+)
