1cd84cc8569c043025b39b404fb698f196d27921
kenriortega <kenriortega@gmail.com>
2021-09-07 20:52:25 -0400

    test: add new test for process getinfo method

--- test/qvapay_test.go
+++ test/qvapay_test.go
@@ -16,7 +16,7 @@ const (
 	secretID = "mySecretID"
 )
 
-func TestInvalidURL(t *testing.T) {
+func Test_Invalid_URL(t *testing.T) {
 
 	client := qvapaygo.NewClient(appID, secretID, "ht&@-tp://:aa", true, nil, nil)
 
@@ -24,6 +24,37 @@ func TestInvalidURL(t *testing.T) {
 	assert.Error(t, err)
 	assert.Empty(t, actual)
 
+}
+
+func Test_Get_Info(t *testing.T) {
+	uuidExpected := "123456789"
+	s := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			time.Sleep(50 * time.Millisecond)
+			w.Write([]byte(
+				` {
+					 	"user_id":1,
+					 	"name":"my_website",
+					 	"url":"https:\/\/www.website.com",
+					 	"desc":"WebSite",
+					 	"callback":"https:\/\/www.website.com\/webhook",
+					 	"logo":"",
+					 	"uuid":"123456789",
+					 	"secret":"123456987",
+					 	"active":1,
+					 	"enabled":1
+					 }`,
+			))
+		}),
+	)
+	defer s.Close()
+
+	client := qvapaygo.NewClient(appID, secretID, s.URL, true, nil, nil)
+	info, err := client.GetInfo(context.Background())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	assert.Equal(t, uuidExpected, info.Uuid)
 }
 
 func Test_Get_Balance(t *testing.T) {
